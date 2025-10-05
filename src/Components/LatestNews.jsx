6ba351803b454a299827047a5496782fa8be9f2d
neamatullah01 <neamat22205101908@diu.edu.bd>
2025-10-05 23:43:37 +0600

    navbar added

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
   return (
-    <div className="w-11/12 mx-auto flex items-center gap-4 bg-base-200 py-2 px-3 my-3">
+    <div className="flex items-center gap-4 bg-base-200 py-2 px-3 my-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
       <Marquee className="flex gap-3" pauseOnHover={true}>
         <p className="font-bold">
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header></Header>
-        <section>
+        <section className="w-11/12 mx-auto">
           <LatestNews></LatestNews>
         </section>
+        <nav className="w-11/12 mx-auto ">
+          <Navbar></Navbar>
+        </nav>
       </header>
       <main>
         <section className="left_nav"></section>
