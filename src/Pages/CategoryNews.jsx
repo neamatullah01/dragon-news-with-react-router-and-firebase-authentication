78f55a6aebab0abf123488a4869267308df78bd3
neamatullah01 <neamat22205101908@diu.edu.bd>
2025-10-06 11:31:15 +0600

    load data for indiviual category

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
+  const [categoryNews, setCategoryNews] = useState([]);
   const { id } = useParams();
-  return <div>CategoryNews {id}</div>;
+  const data = useLoaderData();
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id]);
+  return <div>CategoryNews {categoryNews.length}</div>;
 };
 
 export default CategoryNews;
